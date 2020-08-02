3f1861b98715ab3c4186bc1ab1a554980963aa31
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-08-02 06:33:32 +0530

    feat: jobtype enum

--- src/entity/category.entity.ts
+++ src/entity/category.entity.ts
@@ -2,10 +2,20 @@ import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, OneToOne 
 import { Employee } from './employee.entity'
 import Template from './template.entity'
 
+export enum JobType {
+	FREELANCE = 'freelancer',
+	PART_TIME = 'part-time',
+	FULL_TIME = 'full-time',
+	INTERN = 'intern',
+}
+
 @Entity()
 export class Category extends Template {
-	@Column()
-	job_type: string
+	@Column({
+		type: 'enum',
+		enum: JobType,
+	})
+	job_type: JobType
 
 	@OneToMany((type) => Employee, (employee) => employee.category)
 	employees: Employee[]
